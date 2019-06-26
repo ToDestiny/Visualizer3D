6e3221c1f1bd4672298bdbb2c7889aebf0504644
Alex AMADORI <aamadori@student.42.fr>
2019-06-26 17:22:58 +0200

    switched to orbit controls

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,17 +1,21 @@
 import * as THREE from 'three'
 import { DecalGeometry } from 'three/examples/jsm/geometries/DecalGeometry.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GLTFLoader from 'three-gltf-loader'
+
+function to_radians(angle) {
+    return angle * 2 * Math.PI / 360.0
+}
 
 export default class Renderer {
     constructor(container) {
         this.container = container
         this.rotation_y = 0
         this.rotation_x = 0
-        this.width = 600 // window.innerWidth
-        this.height = 400 // window.innerHeight
+        this.width = 800 // window.innerWidth
+        this.height = 600 // window.innerHeight
         this.view_angle = 75
-        this.is_dragging = false
-        this.mouse_mode = "dragging"
+        this.moved = false
         this.intersection = {
             intersects: false,
             object: null,
@@ -45,64 +49,18 @@ export default class Renderer {
         )
     }
     mouseDown () {
-        if (this.mouse_mode === "dragging")
-            this.is_dragging = true
+        this.moved = false
     }
     mouseUp () {
-        if (this.mouse_mode === "dragging")
-            this.is_dragging = false
-        else if (this.mouse_mode === "shooting")
+        if (!this.moved)
             this.shoot()
     }
     mouseLeave () {
-        if (this.mouse_mode === "dragging")
-            this.is_dragging = false
-    }
-    toggleMouseMode() {
-        if (this.mouse_mode === "dragging")
-        {
-            this.mouse_mode = "shooting"
-            this.is_dragging = false
-        }
-        else if (this.mouse_mode === "shooting")
-            this.mouse_mode = "dragging"
     }
     mouseMove (e) {
-        function toRadians(angle) {
-            return angle * (Math.PI / 180.0);
-        }
-        var deltaMove = {x: 0.0, y: 0.0}
-        if (this.mouse_position)
-        {
-            deltaMove =  {
-                x: e.offsetX - this.mouse_position.x,
-                y: e.offsetY - this.mouse_position.y
-            }
-        }
         this.mouse_position = {
             x: e.offsetX,
             y: e.offsetY,
-        }
-        if (this.is_dragging) {
-            // TODO switch to orbit controls
-            this.rotation_x += deltaMove.y * 0.01
-            this.rotation_y += deltaMove.x * 0.01
-            var vert_limit = toRadians(25)
-            if (this.rotation_x > vert_limit)
-                this.rotation_x = vert_limit
-            else if (this.rotation_x < -vert_limit)
-                this.rotation_x = -vert_limit
-            this.model.setRotationFromEuler(new THREE.Euler(
-                this.rotation_x,
-                this.rotation_y,
-                0,
-                'XYZ'))
-            this.decals.forEach((d) => d.setRotationFromEuler(new THREE.Euler(
-                this.rotation_x,
-                this.rotation_y,
-                0,
-                'XYZ')))
-            this.renderer.render(this.scene, this.camera)
         }
         this.mouse_coords = {
             x: (this.mouse_position.x / this.renderer.domElement.width) * 2 - 1,
@@ -111,28 +69,10 @@ export default class Renderer {
         this.getImpact()
         // console.log(this.mouse_coords)
     }
-    zoomIn(val) {
-        if (val > 0)
-        {
-            this.distance -= val
-            if (this.distance < 1.3)
-                this.distance = 1.3
-            this.render()
-        }
-    }
-    zoomOut(val) {
-        if (val > 0)
-        {
-            this.distance += val
-            if (this.distance > 3.5)
-                this.distance = 3.5
-            this.render()
-        }
-    }
     getImpact() {
         this.raycaster.setFromCamera(this.mouse_coords, this.camera)
         var raycast = this.raycaster.intersectObjects([this.model], true)
-        if (raycast) {
+        if (raycast && raycast.length) {
             this.intersection.intersects = true
             this.intersection.object = raycast[0].object
             this.intersection.point.copy(raycast[0].point)
@@ -146,7 +86,6 @@ export default class Renderer {
         }
         else
             this.intersection.intersects = false
-        this.render()
     }
     shoot() {
         if (this.intersection.intersects) {
@@ -157,7 +96,7 @@ export default class Renderer {
             var decal_geom = new DecalGeometry(this.intersection.object,
                 point, orientation, placeholder_size)
             var decal = new THREE.Mesh(decal_geom, new THREE.MeshBasicMaterial({
-                color: 0x00ff00,
+                map: this.texture_loader.load('/t-shirt_workshop/textures/hanger_maple_baseColor.jpeg'),
                 transparent: true,
                 depthTest: true,
                 depthWrite: false,
@@ -166,19 +105,7 @@ export default class Renderer {
             }))
             this.decals.push(decal)
             this.scene.add(decal)
-            this.render()
         }
-    }
-    initCamera() {
-        const camera = new THREE.PerspectiveCamera(this.view_angle,
-            this.width/this.height, 0.1, 9999)
-        camera.position.z = this.distance
-        camera.lookAt(new THREE.Vector3(0, 0, 0))
-        this.camera = camera
-    }
-    render() {
-        this.initCamera()
-        this.renderer.render(this.scene, this.camera)
     }
     initThree(model) {
         const scene = new THREE.Scene()
@@ -193,12 +120,29 @@ export default class Renderer {
         const renderer = new THREE.WebGLRenderer({antialias: true})
         renderer.setClearColor(0xFFFFFF)
         this.container.appendChild(renderer.domElement)
-        renderer.domElement.onmousedown = this.mouseDown.bind(this)
-        renderer.domElement.onmouseup = this.mouseUp.bind(this)
-        renderer.domElement.onmousemove = this.mouseMove.bind(this)
-        renderer.domElement.onmouseleave = this.mouseLeave.bind(this)
         renderer.setSize(this.width, this.height)
 
+        const camera = new THREE.PerspectiveCamera(this.view_angle,
+            this.width/this.height, 0.1, 9999)
+        camera.position.copy(new THREE.Vector3(0.5, 0.5, 3))
+        const controls = new OrbitControls(camera, renderer.domElement)
+        controls.minDistance = 1.5
+        controls.maxDistance = 10
+        controls.maxPolarAngle = to_radians(90 + 40)
+        controls.minPolarAngle = to_radians(90 - 40)
+        controls.enableKeys = false
+        controls.enablePan = false
+
+        controls.addEventListener('change', function () {
+            this.moved = true
+        }.bind(this))
+        renderer.domElement.addEventListener('mousedown', this.mouseDown.bind(this))
+        renderer.domElement.addEventListener('mouseup', this.mouseUp.bind(this))
+        renderer.domElement.addEventListener('mousemove', this.mouseMove.bind(this))
+        renderer.domElement.addEventListener('mouseleave', this.mouseLeave.bind(this))
+
+        this.texture_loader = new THREE.TextureLoader()
+        this.camera = camera
         this.renderer = renderer
         this.scene = scene
         this.model = model
@@ -212,6 +156,10 @@ export default class Renderer {
         var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
         var mesh = new THREE.Mesh( geometry, material );
         scene.add(geometry*/
-        this.render()
+        this.renderLoop()
+    }
+    renderLoop() {
+        window.requestAnimationFrame(this.renderLoop.bind(this))
+        this.renderer.render(this.scene, this.camera)
     }
 }
