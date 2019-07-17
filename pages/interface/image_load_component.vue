4ea42791be571466b108c23d7fb32445b0c5da4b
Alex Amadori <alexamadori592@gmail.com>
2019-07-17 18:02:26 +0200

    made interface for adding logos prettier

--- pages/interface/image_load_component.vue
+++ pages/interface/image_load_component.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="mt-3 mb-3 d-flex flex-row justify-content-start" id="dz" @drop="onDrop" @dragover="onDragHandler">
+    <div class="mt-3 mb-3 d-flex flex-row justify-content-start flex-wrap" id="dz" @drop="onDrop" @dragover="onDragHandler">
         <font-awesome-icon style="font-size: 4em;" icon="plus-square" @click="clickOnTmpFile"/>
         <p v-if="image_count == 0">Drag your logos here</p>
-        <div class="image_up" v-for="(img_info, uuid) in images" :key="uuid">
-            <img :src="img_info.data" alt="** Preview **" :ref="'img_' + uuid" height="65" width="65"/>
-            <br />
+        <div class="inline-block" v-for="(img_info, uuid) in images" :key="uuid">
+            <div class="logo_container">
+                <img class="logo_thumb" :src="img_info.data" alt="Logo Preview" :ref="'img_' + uuid"/>
+                <button class="delete-button" @click.prevent.stop="deleteImage(uuid)">
+                    <font-awesome-icon icon="window-close"/>
+                </button>
+            </div>
             <div>
                 {{ img_info.file.name.substring(0, 13) }}
             </div>
@@ -13,9 +17,6 @@
                     {{ pos.name }}
                 </option>
             </select>
-            <button @click.prevent.stop="deleteImage(uuid)">
-                <font-awesome-icon icon="window-close"/>
-            </button>
         </div>
         <input @change="newTmpFile" type="file" ref="tmpFile" style="display: none;" />
     </div>
@@ -24,4 +25,29 @@
 <script src="./image_load_component.js"></script>
 
 <style>
+.logo_container {
+    position: relative;
+    max-width: 150px;
+    height: 70px;
+}
+.logo_thumb {
+    max-width:100%;
+    max-height:100%;
+    margin: 0;
+}
+.delete-button {
+    border-style: solid;
+    line-height: 1em;
+    padding: 0;
+    margin: 0;
+}
+.logo_container .delete-button {
+    position: absolute;
+    top: 0%;
+    right: 0%;
+}
+.delete-button * {
+    margin: 0;  
+    font-size: 1em;
+}
 </style>
